ad299d2087a20d5305278a2821c9014ff84cd2ff
Jan Willhaus <mail@janwillhaus.de>
2019-02-25 22:21:45 +0100

    Adjust formatting

--- tests/test_influxdb.py
+++ tests/test_influxdb.py
@@ -3,11 +3,13 @@ from piholeinflux import send_msg
 
 def test_send_msg(mocker):
     indata = {"some": "value", "gravity_last_updated": "should be gone"}
-    expected = [{
-        "measurement": "pihole",
-        "tags": {"host": "myname"},
-        "fields": {"some": "value"},
-    }]
+    expected = [
+        {
+            "measurement": "pihole",
+            "tags": {"host": "myname"},
+            "fields": {"some": "value"},
+        }
+    ]
     mock_influx = mocker.patch("influxdb.InfluxDBClient")
 
     send_msg(mock_influx(), indata, "myname")
