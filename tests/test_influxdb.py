fccff36dd14c5b8612dc6460a8ab83bb482bd1e8
Jan Willhaus <mail@janwillhaus.de>
2019-03-02 21:18:36 +0100

    Close #9: ads_percentage_today cast as integer when 0 (#10)
    * Close #9: ads_percentage_today cast as integer when 0
    * Tests for type enforcement in ads_percentage_today
    * Fix run of coveralls

--- tests/test_influxdb.py
+++ tests/test_influxdb.py
@@ -8,7 +8,33 @@ def test_send_msg(mocker):
         {
             "measurement": "pihole",
             "tags": {"host": "myname"},
-            "fields": {"some": "value"},
+            "fields": {"some": "value", "ads_percentage_today": 0.0},
+        }
+    ]
+    mock_influx = mocker.patch("influxdb.InfluxDBClient")
+
+    send_msg(mock_influx(), indata, "myname")
+
+    mock_influx().write_points.assert_called_once_with(expected)
+
+
+def test_send_msg_integer(mocker):
+    """Test proper conversion of ads_percentage_today to float."""
+
+    def IsOfType(cls):
+        class IsOfType(cls):
+            def __eq__(self, other):
+                return isinstance(other, cls)
+
+        return IsOfType()
+
+    """Test send_msg function, sending data to influxDB."""
+    indata = {"ads_percentage_today": 0, "gravity_last_updated": "should be gone"}
+    expected = [
+        {
+            "measurement": "pihole",
+            "tags": {"host": "myname"},
+            "fields": {"ads_percentage_today": IsOfType(float)},
         }
     ]
     mock_influx = mocker.patch("influxdb.InfluxDBClient")
