52dc3e1e2f6d5757f3c51bfcaa12f584fa34f158
Jan Willhaus <mail@janwillhaus.de>
2020-05-12 12:07:57 +0200

    Fix tests

--- tests/test_influxdb.py
+++ tests/test_influxdb.py
@@ -3,44 +3,11 @@ from piholeinflux import Daemon
 
 def test_send_msg(mocker):
     """Test send_msg function, sending data to influxDB."""
-    indata = {"some": "value", "gravity_last_updated": "should be gone"}
-    expected = [
-        {
-            "measurement": "pihole",
-            "tags": {"host": "myname"},
-            "fields": {"some": "value", "ads_percentage_today": 0.0},
-        }
-    ]
+    data = {"some": "value", "gravity_last_updated": "is not modified"}
+    expected = [{"measurement": "pihole", "tags": {"host": "myname"}, "fields": data}]
     mock_influx = mocker.patch("piholeinflux.InfluxDBClient")
     d = Daemon()
-    d.send_msg(indata, "myname")
-
-    mock_influx().write_points.assert_called_with(expected)
-    assert mock_influx().write_points.call_count == 1
-
-
-def test_send_msg_integer(mocker):
-    """Test proper conversion of ads_percentage_today to float."""
-
-    def IsOfType(cls):
-        class IsOfType(cls):
-            def __eq__(self, other):
-                return isinstance(other, cls)
-
-        return IsOfType()
-
-    """Test send_msg function, sending data to influxDB."""
-    indata = {"ads_percentage_today": 0, "gravity_last_updated": "should be gone"}
-    expected = [
-        {
-            "measurement": "pihole",
-            "tags": {"host": "myname"},
-            "fields": {"ads_percentage_today": IsOfType(float)},
-        }
-    ]
-    mock_influx = mocker.patch("piholeinflux.InfluxDBClient")
-    d = Daemon()
-    d.send_msg(indata, "myname")
+    d.send_msg(data, "myname")
 
     mock_influx().write_points.assert_called_with(expected)
     assert mock_influx().write_points.call_count == 1
--- tests/test_pihole.py
+++ tests/test_pihole.py
@@ -2,6 +2,31 @@ import pytest
 from piholeinflux import Pihole
 
 
+PAYLOAD_FIXTURE = {
+    "domains_being_blocked": 86424,
+    "dns_queries_today": 222897,
+    "ads_blocked_today": 1716,
+    "ads_percentage_today": 0,
+    "unique_domains": 2326,
+    "queries_forwarded": 20657,
+    "queries_cached": 200516,
+    "clients_ever_seen": 24,
+    "unique_clients": 24,
+    "dns_queries_all_types": 222897,
+    "reply_NODATA": 181,
+    "reply_NXDOMAIN": 6,
+    "reply_CNAME": 49,
+    "reply_IP": 784,
+    "privacy_level": 0,
+    "status": "enabled",
+    "gravity_last_updated": {
+        "file_exists": True,
+        "absolute": 1589269042,
+        "relative": {"days": 0, "hours": 0, "minutes": 6},
+    },
+}
+
+
 def test_pihole_init():
     """Test object initialization."""
     config = {"name": "pihole", "url": "http://here.example"}
@@ -15,6 +40,7 @@ def test_pihole_init():
     assert pihole.name == "pihole"
     assert pihole.url == "http://here.example"
     assert pihole.timeout == 10
+    assert pihole.verify_ssl is True
 
 
 @pytest.mark.vcr()
@@ -28,3 +54,30 @@ def test_pihole_get_data():
     assert "domains_being_blocked" in response
     assert "ads_percentage_today" in response
     assert "gravity_last_updated" in response
+
+
+def test_data_sanitizer():
+    """Test proper sanitizing of data."""
+
+    def IsOfType(cls):
+        class IsOfType(cls):
+            def __eq__(self, other):
+                return isinstance(other, cls)
+
+        return IsOfType()
+
+    data = Pihole.sanitize_payload(PAYLOAD_FIXTURE)
+
+    assert data is not PAYLOAD_FIXTURE
+    assert "domains_being_blocked" in data
+    assert "ads_percentage_today" in data
+    assert "gravity_last_updated" in data
+    assert isinstance(data["ads_percentage_today"], float)
+    assert isinstance(data["gravity_last_updated"], int)
+
+
+def test_data_sanitizer_last_updated_dict():
+    payload = {"gravity_last_updated": "we don't want this"}
+    data = Pihole.sanitize_payload(payload)
+
+    assert len(data) == 0
